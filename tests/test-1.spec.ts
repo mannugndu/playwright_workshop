499416deb1b06d85f0ba542386f2c8bcbf7c57ef
Manpreet Singh <mannu.gndu@gmail.com>
2023-12-06 15:01:15 +0530

    click first submit

--- tests/test-1.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('http://localhost:8080/');
-  await page.getByText('Mow your lawn').click();
-  await expect(page.locator('#activity')).toContainText('Mow your lawn');
-});
